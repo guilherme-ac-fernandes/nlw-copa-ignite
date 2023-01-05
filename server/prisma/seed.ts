3aaaa3cd90110056a39ceb917a658a055be24d8b
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-05 17:56:42 -0300

    feat: criação do seed do banco de dados

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Guilherme Fernandes',
+      email: 'gfengquimica@gmail.com',
+      avatarUrl: 'https://github.com/guilherme-ac-fernandes.png',
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'BOL123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: '2023-01-04T20:49:19.984Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: '2023-01-05T20:50:03.172Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
