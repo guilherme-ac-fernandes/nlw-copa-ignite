c7970734dc98ac44ea9adadbe8ea1b6b4696ace1
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-07 02:17:52 -0300

    feat: estruturação inicial da página de pools

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useState } from 'react';
-import { VStack, Icon, useToast, FlatList } from 'native-base';
+// import { useCallback, useState } from 'react';
+import { VStack, Icon } from 'native-base';
 import { Octicons } from '@expo/vector-icons';
-import { useNavigation, useFocusEffect } from '@react-navigation/native';
+// import { useNavigation, useFocusEffect } from '@react-navigation/native';
 
 // API
 import { api } from '../services/api';
@@ -9,42 +9,32 @@ import { api } from '../services/api';
 // Components
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
-import { PoolCard, PoolCardPros } from '../components/PoolCard';
-import { Loading } from '../components/Loading';
-import { EmptyPoolList } from '../components/EmptyPoolList';
+// import { PoolCard, PoolCardPros } from '../components/PoolCard';
+// import { Loading } from '../components/Loading';
+// import { EmptyPoolList } from '../components/EmptyPoolList';
 
 export function Pools() {
-  
-
   return (
-    <VStack flex={1} bgColor="gray.900">
-      <Header title="Meus Bolões" />
+    <VStack flex={1} bgColor='gray.900'>
+      <Header title='Meus Bolões' />
 
-      <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
-        <Button 
-          title="BUSCAR BOLÃO POR CÓDIGO" 
-          leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
+      <VStack
+        mt={6}
+        mx={5}
+        borderBottomWidth={1}
+        borderBottomColor='gray.600'
+        pb={4}
+        mb={4}
+      >
+        <Button
+          title='BUSCAR BOLÃO POR CÓDIGO'
+          leftIcon={
+            <Icon as={Octicons} name='search' color='black' size='md' />
+          }
           // onPress={() => navigate('find')}
         />
       </VStack>
 
-      {/* {
-        isLoading ? <Loading /> :
-          <FlatList 
-            data={pools}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => (
-              <PoolCard 
-                data={item}
-                onPress={() => navigate('details', { id: item.id })}
-              />
-            )}
-            ListEmptyComponent={<EmptyPoolList />}
-            showsVerticalScrollIndicator={false}
-            _contentContainerStyle={{ pb: 10 }}
-            px={5}
-          />
-      } */}
     </VStack>
   );
 }
