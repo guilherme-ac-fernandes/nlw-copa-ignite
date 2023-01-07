ca8e8cb2289587c0610bbaf3e231f7017a5704ab
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-07 01:25:12 -0300

    feat: adiciona componentes já criados para facilitar

---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { Heading, Text, VStack, useToast } from 'native-base';
+
+export function New() {
+  return (
+    <VStack flex={1} bg='gray.900'>
+      
+    </VStack>
+  );
+}
